d381ba51c703d89db7e4b44dd767559ba53e3a4f
Alexey141 <78477954+Alexey141@users.noreply.github.com>
2021-03-15 12:57:00 +0800

    Добавление кнопок для работы с двумя двигателями.

--- Mdrive/Mdrive.py
+++ Mdrive/Mdrive.py
@@ -11,19 +11,21 @@ ui = Ui_Dialog()
 ui.setupUi(Dialog)
 Dialog.show()
 
-#ip = ui.lineEdit_2.text()
-#port = ui.lineEdit_3.text()
-
 #Подключение к двигателю
 def connections():
     try:
         global s
+        #ip = ui.lineEdit_2.text()
+        #port = ui.lineEdit_3.text()
         s = socket.socket(socket.AF_INET)
+        s.settimeout(1)
         s.connect(('192.168.0.7', 5000))
-        s.setblocking(False)
-        prnt("Подключились к сокету.")
+
+        s.setblocking(True)
+        prnt("Подключились к сокету")
     except:
         prnt("Ошибка, проверьте правильность данных.")
+
 ui.pushButton.clicked.connect( connections )
 
 # Функция для занесения данных в textEdit
@@ -34,43 +36,99 @@ def prnt(s):
 def send():
     try:
         text = ui.lineEdit.text()
-        s.send("\n".encode("utf-8") + text.encode("utf-8") + "\n".encode("utf-8"))
+        name = ui.lineEdit_5.text()
+        s.send("\n".encode("utf-8") + name.encode("utf-8") + text.encode("utf-8") + "\n".encode("utf-8"))
+        this.socket.setSoTimeout(timeOut)
         time.sleep(0.5)
         data = s.recv(1024)
-        prnt ( "\n" + str(data) + "\n")
-        prnt("\nЗапрос отправлен \n")
+        prnt ( "\n" + str(data.decode()) + "\n")
+        prnt("Запрос отправлен \n")
     except:
-        prnt("\nПроверьте правильность команды \n")
+        prnt("\nПроверьте правильность команды. \n")
 
 ui.pushButton_2.clicked.connect( send )
 
-#Движение двигателя влево
+#Движение по оси -x
 def left():
-    Speed = ui.lineEdit_4.text()
-    s.send('\nX SL '.encode("utf-8") + Speed.encode("utf-8") + '\n'.encode("utf-8"))
-    prnt("\nДвижение началось\n")
+    try:
+        global Speed
+        Speed = ui.lineEdit_4.text()
+        s.send("\n".encode("utf-8") + "X SL -".encode("utf-8") + Speed.encode("utf-8") + "\n".encode("utf-8"))
+        otvet = s.recv(1024)
+        prnt ( "\n" + str(otvet) + "\n")
+        prnt("\nДвижение началось\n")
+    except:
+        prnt("\nУстройство не подключено\n")
 
 ui.pushButton_4.clicked.connect( left )
 
-#Движение двигателя вправо
-#def right():
-#    Speed = ui.lineEdit_4.text()
-#    s.send('\nX SL'.encode("utf-8") + Speed.encode("utf-8") + '\n'.encode("utf-8"))
-#    prnt("\nДвижение против часовой стрелки\n")
+#Движение по оси +y
+def up():
+    try:
+        s.send("\n".encode("utf-8") + "Y SL ".encode("utf-8") + Speed.encode("utf-8") + "\n".encode("utf-8"))
+        otvet = s.recv(1024)
+        prnt ( "\n" + str(otvet) + "\n")
+        prnt("\nДвижение началось\n")
+    except:
+        prnt("\nУстройство не подключено\n")
 
-#ui.pushButton_5.clicked.connect( right )
+ui.pushButton_5.clicked.connect( up )
 
-def STOP():
-    s.send(b'\nX SL 0\n')
-    prnt("\nДвижение остановилось\n")
+#Движение по оси +x
+def right():
+    try:
+        s.send("\n".encode("utf-8") + "X SL ".encode("utf-8") + Speed.encode("utf-8") + "\n".encode("utf-8"))
+        otvet = s.recv(1024)
+        prnt ( "\n" + str(otvet) + "\n")
+        prnt("\nДвижение началось\n")
+    except:
+        prnt("\nУстройство не подключено\n")
 
-ui.pushButton_6.clicked.connect( STOP )
+ui.pushButton_7.clicked.connect( right )
+
+#Движение по оси -y
+def down():
+    try:
+        s.send("\n".encode("utf-8") + "Y SL -".encode("utf-8") + Speed.encode("utf-8") + "\n".encode("utf-8"))
+        otvet = s.recv(1024)
+        prnt ( "\n" + str(otvet) + "\n")
+        prnt("\nДвижение началось\n")
+    except:
+        prnt("\nУстройство не подключено\n")
+
+ui.pushButton_8.clicked.connect( down )
+
+# Остановка двигателя
+def STOPX():
+    try:
+        s.send(b"\nX SL 0\n")
+        otvet = s.recv(1024)
+        prnt ( "\n" + str(otvet) + "\n")
+        prnt("\nДвижение остановилось\n")
+    except:
+        prnt("\nУстройство не подключено\n")
+
+ui.pushButton_6.clicked.connect( STOPX )
+
+# Остановка двигателя
+def STOPY():
+    try:
+        s.send(b"\nY SL 0\n")
+        otvet = s.recv(1024)
+        prnt ( "\n" + str(otvet) + "\n")
+        prnt("\nДвижение остановилось\n")
+    except:
+        prnt("\nУстройство не подключено\n")
+
+ui.pushButton_9.clicked.connect( STOPY )
 
 #Отключение от сокета
 def EXIT():
-    s.close()
-    prnt("\nВы отключились от устройства. \n")
-
+    try:
+        s.close()
+        prnt("\nВы отключились от устройства. \n")
+    except:
+        prnt("\nВы не подключены к устройству\n")
 ui.pushButton_3.clicked.connect( EXIT )
 
 app.exec()
--- Mdrive/MdriveGUI.py
+++ Mdrive/MdriveGUI.py
@@ -3,71 +3,150 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
-        Dialog.resize(600, 600)
-        Dialog.setStyleSheet("background-color: blacks;")
+        Dialog.resize(600, 650)
+        Dialog.setStyleSheet("background-color: green;")
+
+# Подключение к устройству (connection)
         self.pushButton = QtWidgets.QPushButton(Dialog)
-        self.pushButton.setGeometry(QtCore.QRect(50, 510, 120, 30))
-        self.pushButton.setStyleSheet("background-color:#FF0000;\n"
+        self.pushButton.setGeometry(QtCore.QRect(180, 570, 120, 35))
+        self.pushButton.setStyleSheet("background-color:#A62D00;\n"
 "font: 75 10pt \"Comic Sans MS\";")
         self.pushButton.setObjectName("pushButton")
 
+# Отправление запроса (Send)
         self.pushButton_2 = QtWidgets.QPushButton(Dialog)
-        self.pushButton_2.setGeometry(QtCore.QRect(180, 510, 120, 30))
-        self.pushButton_2.setStyleSheet("background-color:#FF0000;\n"
+        self.pushButton_2.setGeometry(QtCore.QRect(260, 520, 120, 35))
+        self.pushButton_2.setStyleSheet("background-color:#BF5730;\n"
 "font: 75 10pt \"Comic Sans MS\";")
         self.pushButton_2.setObjectName("pushButton_2")
 
+# Отключение от устройства (EXIT)
         self.pushButton_3 = QtWidgets.QPushButton(Dialog)
-        self.pushButton_3.setGeometry(QtCore.QRect(120, 560, 120, 30))
-        self.pushButton_3.setStyleSheet("background-color:#FF0000;\n"
+        self.pushButton_3.setGeometry(QtCore.QRect(320, 570, 120, 35))
+        self.pushButton_3.setStyleSheet("background-color:#A62D00;\n"
 "font: 75 10pt \"Comic Sans MS\";")
         self.pushButton_3.setObjectName("pushButton_3")
 
+# Направление вверх (👆)
         self.pushButton_4 = QtWidgets.QPushButton(Dialog)
-        self.pushButton_4.setGeometry(QtCore.QRect(360, 510, 100, 40))
-        self.pushButton_4.setStyleSheet("background-color:#FF0000;\n"
-"font: 75 14pt \"Comic Sans MS\";")
+        self.pushButton_4.setGeometry(QtCore.QRect(400, 400, 40, 40))
+        self.pushButton_4.setStyleSheet("background-color:#BF5730;\n"
+"font: 75 15pt \"Comic Sans MS\";")
         self.pushButton_4.setObjectName("pushButton_4")
 
-#        self.pushButton_5 = QtWidgets.QPushButton(Dialog)
-#        self.pushButton_5.setGeometry(QtCore.QRect(500, 510, 40, 40))
-#        self.pushButton_5.setStyleSheet("background-color:#FF0000;\n"
-#"font: 75 14pt \"Comic Sans MS\";")
-#        self.pushButton_5.setObjectName("pushButton_5")
+# Направление вправо (👉)
+        self.pushButton_5 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_5.setGeometry(QtCore.QRect(450, 450, 40, 40))
+        self.pushButton_5.setStyleSheet("background-color:#BF5730;\n"
+"font: 75 15pt \"Comic Sans MS\";")
+        self.pushButton_5.setObjectName("pushButton_5")
 
+# Остановка движения (█)
         self.pushButton_6 = QtWidgets.QPushButton(Dialog)
-        self.pushButton_6.setGeometry(QtCore.QRect(470, 510, 90, 40))
-        self.pushButton_6.setStyleSheet("background-color:#FF0000;\n"
-"font: 75 14pt \"Comic Sans MS\";")
+        self.pushButton_6.setGeometry(QtCore.QRect(400, 450, 40, 20))
+        self.pushButton_6.setStyleSheet("background-color:#BF5730;\n"
+"font: 75 12pt \"Comic Sans MS\";")
         self.pushButton_6.setObjectName("pushButton_6")
 
+# Движение влево (👈)
+        self.pushButton_7 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_7.setGeometry(QtCore.QRect(350, 450, 40, 40))
+        self.pushButton_7.setStyleSheet("background-color:#BF5730;\n"
+"font: 75 15pt \"Comic Sans MS\";")
+        self.pushButton_7.setObjectName("pushButton_5")
+
+# Движение вниз (👇)
+        self.pushButton_8 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_8.setGeometry(QtCore.QRect(400, 500, 40, 40))
+        self.pushButton_8.setStyleSheet("background-color:#BF5730;\n"
+"font: 75 15pt \"Comic Sans MS\";")
+        self.pushButton_8.setObjectName("pushButton_5")
+
+# Остановка движения (█)
+        self.pushButton_9 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_9.setGeometry(QtCore.QRect(400, 470, 40, 20))
+        self.pushButton_9.setStyleSheet("background-color:#BF5730;\n"
+"font: 75 12pt \"Comic Sans MS\";")
+        self.pushButton_9.setObjectName("pushButton_6")
+
+#
+        self.textEdit = QtWidgets.QTextEdit(Dialog)
+        self.textEdit.insertPlainText("Порт")
+        self.textEdit.setGeometry(QtCore.QRect(10, 400, 70, 30))
+        self.textEdit.setObjectName("textEdit")
+
+        self.textEdit_2 = QtWidgets.QTextEdit(Dialog)
+        self.textEdit_2.insertPlainText("Адрес")
+        self.textEdit_2.setGeometry(QtCore.QRect(10, 440, 70, 30))
+        self.textEdit_2.setObjectName("textEdit_2")
+
+        self.textEdit_3 = QtWidgets.QTextEdit(Dialog)
+        self.textEdit_3.insertPlainText("Имя")
+        self.textEdit_3.setGeometry(QtCore.QRect(10, 480, 70, 30))
+        self.textEdit_3.setObjectName("textEdit_3")
+
+        self.textEdit_4 = QtWidgets.QTextEdit(Dialog)
+        self.textEdit_4.insertPlainText("Запрос")
+        self.textEdit_4.setGeometry(QtCore.QRect(10, 520, 70, 35))
+        self.textEdit_4.setObjectName("textEdit_4")
+
+        self.textEdit = QtWidgets.QTextEdit(Dialog)
+        self.textEdit.insertPlainText("PY")
+        self.textEdit.setGeometry(QtCore.QRect(260, 400, 30, 30))
+        self.textEdit.setObjectName("textEdit")
+
+        self.checkBox = QtWidgets.QCheckBox(Dialog)
+        self.checkBox.setGeometry(QtCore.QRect(295, 405, 20, 20))
+        self.checkBox.setObjectName("checkBox")
+
+#        self.comboBox = QtWidgets.QComboBox(Dialog)
+#        self.comboBox.setGeometry(QtCore.QRect(510, 450, 70, 35))
+#        self.comboBox.setStyleSheet("background-color: white;")
+#        self.comboBox.setObjectName("white")
+#        self.comboBox.addItem("")
+#        self.comboBox.addItem("")
+
+# Поле для заполнения запроса
         self.lineEdit = QtWidgets.QLineEdit(Dialog)
-        self.lineEdit.setGeometry(QtCore.QRect(50, 400, 500, 35))
+        self.lineEdit.setGeometry(QtCore.QRect(95, 520, 155, 35))
         self.lineEdit.setStyleSheet("background-color: white; font-size: 25px;")
+        self.lineEdit.setPlaceholderText("#X PR AL")
         self.lineEdit.setObjectName("lineEdit")
 
+# Поле для ip
         self.lineEdit_2 = QtWidgets.QLineEdit(Dialog)
-        self.lineEdit_2.setGeometry(QtCore.QRect(50, 450, 200, 35))
+        self.lineEdit_2.setGeometry(QtCore.QRect(95, 440, 155, 35))
         self.lineEdit_2.setStyleSheet("background-color: white; font-size: 25px;")
-        self.lineEdit_2.setPlaceholderText("IP")
+        self.lineEdit_2.setPlaceholderText("192.168.0.7")
         self.lineEdit_2.setObjectName("lineEdit_2")
 
+# Поле для port'a
         self.lineEdit_3 = QtWidgets.QLineEdit(Dialog)
-        self.lineEdit_3.setGeometry(QtCore.QRect(270, 450, 100, 35))
+        self.lineEdit_3.setGeometry(QtCore.QRect(95, 400, 155, 35))
         self.lineEdit_3.setStyleSheet("background-color: white; font-size: 25px;")
-        self.lineEdit_3.setPlaceholderText("Port")
+        self.lineEdit_3.setPlaceholderText("5000")
         self.lineEdit_3.setObjectName("lineEdit_3")
 
+# Поле для скорости
         self.lineEdit_4 = QtWidgets.QLineEdit(Dialog)
-        self.lineEdit_4.setGeometry(QtCore.QRect(380, 450, 120, 35))
+        self.lineEdit_4.setGeometry(QtCore.QRect(470, 520, 120, 35))
         self.lineEdit_4.setStyleSheet("background-color: white; font-size: 25px;")
         self.lineEdit_4.setPlaceholderText("Speed")
         self.lineEdit_4.setObjectName("lineEdit_4")
 
+# Поле для заполнения запроса
+        self.lineEdit_5 = QtWidgets.QLineEdit(Dialog)
+        self.lineEdit_5.setGeometry(QtCore.QRect(95, 480, 155, 35))
+        self.lineEdit_5.setStyleSheet("background-color: white; font-size: 25px;")
+        self.lineEdit_5.setPlaceholderText("X")
+        self.lineEdit_5.setObjectName("lineEdit")
+
+# Поле вывода
         self.textEdit = QtWidgets.QTextEdit(Dialog)
         self.textEdit.setGeometry(QtCore.QRect(10, 20, 580, 360))
         self.textEdit.setStyleSheet("background: white; font-size: 25px;")
         self.textEdit.setObjectName("textEdit")
+        self.textEdit.setAutoFormatting(QtWidgets.QTextEdit.AutoAll)
 
         self.retranslateUi(Dialog)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
@@ -78,6 +157,12 @@ class Ui_Dialog(object):
         self.pushButton.setText(_translate("Dialog", "Connection"))
         self.pushButton_2.setText(_translate("Dialog", "Send"))
         self.pushButton_3.setText(_translate("Dialog", "EXIT"))
-        self.pushButton_4.setText(_translate("Dialog", "START"))
-        #self.pushButton_5.setText(_translate("Dialog", ">"))
-        self.pushButton_6.setText(_translate("Dialog", "STOP"))
+        self.pushButton_4.setText(_translate("Dialog", "👆"))
+        self.pushButton_5.setText(_translate("Dialog", "👉"))
+        self.pushButton_6.setText(_translate("Dialog", "█"))
+        self.pushButton_7.setText(_translate("Dialog", "👈"))
+        self.pushButton_8.setText(_translate("Dialog", "👇"))
+        self.pushButton_9.setText(_translate("Dialog", "█"))
+
+#        self.comboBox.setItemText(0, _translate("Dialog", "X"))
+#        self.comboBox.setItemText(1, _translate("Dialog", "Y"))
